62c7f3a623af1b36db0d478689080c7163b687c5
Shashank J <shashankreddy231@gmail.com>
2021-01-30 21:44:26 +0530

    Enco Sample Classification on Iris dataset.

--- out/test/WekaJavaProjects/src/encog/EncogIris.java
+++ out/test/WekaJavaProjects/src/encog/EncogIris.java
@@ -1,13 +1,22 @@
 package encog;
 
+import org.encog.ConsoleStatusReportable;
+import org.encog.ml.MLRegression;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.versatile.NormalizationHelper;
 import org.encog.ml.data.versatile.VersatileMLDataSet;
 import org.encog.ml.data.versatile.columns.ColumnDefinition;
 import org.encog.ml.data.versatile.columns.ColumnType;
 import org.encog.ml.data.versatile.sources.CSVDataSource;
 import org.encog.ml.data.versatile.sources.VersatileDataSource;
+import org.encog.ml.factory.MLMethodFactory;
+import org.encog.ml.model.EncogModel;
 import org.encog.util.csv.CSVFormat;
+import org.encog.util.csv.ReadCSV;
+import org.encog.util.simple.EncogUtility;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class EncogIris {
     public static void main(String[] args) {
@@ -22,5 +31,48 @@ public class EncogIris {
 
         ColumnDefinition outputColumn = data.defineSourceColumn("species", 4, ColumnType.nominal);
         data.analyze();
+
+        data.defineSingleOutputOthersInput(outputColumn);
+        EncogModel model = new EncogModel(data);
+        model.selectMethod(data, MLMethodFactory.TYPE_FEEDFORWARD);
+        model.setReport(new ConsoleStatusReportable());
+        data.normalize();
+
+        System.out.println("Normalization helper : ");
+        NormalizationHelper helper = data.getNormHelper();
+        System.out.println(helper.toString());
+
+        model.holdBackValidation(0.3, true, 1001);
+        model.selectTrainingType(data);
+        MLRegression bestMethod = (MLRegression) model.crossvalidate(5, true);
+        System.out.println("Model selected : " + bestMethod.toString());
+
+        System.out.println("Training error : " + EncogUtility.calculateRegressionError(bestMethod, model.getTrainingDataset()));
+        System.out.println("Validation error : " + EncogUtility.calculateRegressionError(bestMethod, model.getValidationDataset()));
+
+        // now predict the values using following code block
+        ReadCSV csv = new ReadCSV(irisFile, false, CSVFormat.DECIMAL_POINT);
+        String[] line = new String[4];
+        MLData input = helper.allocateInputVector();
+
+        while (csv.next()) {
+            StringBuilder result = new StringBuilder();
+            line[0] = csv.get(0);
+            line[1] = csv.get(1);
+            line[2] = csv.get(2);
+            line[3] = csv.get(3);
+            String correct = csv.get(4);
+            helper.normalizeInputVector(line, input.getData(), false);
+            MLData output = bestMethod.compute(input);
+            String irisChosen = helper.denormalizeOutputVectorToString(output)[0];
+
+            result.append(Arrays.toString(line));
+            result.append(" -> predicted: ");
+            result.append(irisChosen);
+            result.append("(correct: ");
+            result.append(correct);
+            result.append(")");
+            System.out.println(result.toString());
+        }
     }
 }
